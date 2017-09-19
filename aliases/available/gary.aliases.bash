ae6f2d4fa6fdc67f2a5f72efdb8e1b66cc6a568b
Gary Camblin <gcamblin@gmail.com>
2017-09-19 10:45:44 +0100

    adding Gary alias

---
+++ aliases/available/gary.aliases.bash
@@ -0,0 +1,6 @@
+cite 'about-alias'
+about-alias 'Gary's iManage commands'
+
+# Aliases
+alias lo5='sudo openconnect --verbose --user gcamblin https://206.25.45.10:4443'
+alias ch3='sudo openconnect --verbose --user gcamblin vpn2-ch3.imanage.work'
